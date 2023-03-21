95f369376194856200838a8453859e01b9f7fdd7
Your Name <you@example.com>
2023-03-21 23:08:02 +0100

    upd

--- simple_kube_dag.py
+++ simple_kube_dag.py
@@ -1,9 +1,14 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
+import time
 
-def helloworld():
-    print('Hello World!')
+def helloworld1():
+    print('Hello World - 1')
+
+def helloworld2():
+    print("The time of code execution begin is : ", time.ctime())
+    print('Hello World - 2')
 
 with DAG(dag_id="hello",
          start_date=datetime(2021,1,1),
@@ -11,7 +16,11 @@ with DAG(dag_id="hello",
          catchup=False) as dag:
 
          task1 = PythonOperator(
-             task_id="hello",
-             python_callable=helloworld)
+             task_id="hello2",
+             python_callable=helloworld1)
 
-task1
+         task2 = PythonOperator(
+             task_id="hello2",
+             python_callable=helloworld2)
+         
+task1 >> task2
