4f13e3714e39dc0be562b8414fb5c055f6a9a2b9
Your Name <you@example.com>
2023-03-21 23:57:50 +0100

    upd

--- pod_dag.py
+++ pod_dag.py
@@ -10,8 +10,7 @@ default_args = {
     "start_date": datetime(2022, 1, 1),
     "email_on_failure": False,
     "email_on_retry": False,
-    "retries": 1,
-    "retry_delay": duration(minutes=1),
+    "retries": 1
 }
 
 namespace = conf.get("kubernetes", "NAMESPACE")
