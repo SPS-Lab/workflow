5402fdd879d8bd76ea41b67cf5a7a68137cdd0a3
gabin-s <gabins@kth.se>
2023-03-31 09:16:10 +0200

    added workflow for autogrid

---
+++ autogrid.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.models.param import Param
+from airflow.decorators import dag, task
+from airflow.configuration import conf
+
+from datetime import datetime
+
+default_args = {}
+namespace = conf.get('kubernetes', 'NAMESPACE')
+
+@task
+def detect_receptor_types():
+    pass
+
+@task
+def prepare_grid():
+    return None
+
+@dag(start_date=datetime(2021, 1, 1),
+     schedule=None,
+     catchup=False,
+     default_args=default_args)
+def autogrid():
+    prepare_grid()
+
+autodock()
