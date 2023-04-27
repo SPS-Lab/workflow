934badc277ebf0d6c9f2acd602a74eaeb374df26
Gabin Schieffer <gabin.schieffer@gmail.com>
2023-04-28 00:28:05 +0200

    test

--- test_dag.py
+++ test_dag.py
@@ -38,23 +38,21 @@ def test_dag():
         do_xcom_push=True,
     )
 
-    @task_group
-    def docking(batch_label: str):
-        perform_docking = KubernetesPodOperator(
-            namespace=namespace,
-            task_id='perform_docking',
-            image="alpine",
-            cmds=["sh", "-c", f'echo docking: barabra'],
-        )
+    perform_docking = KubernetesPodOperator(
+        namespace=namespace,
+        task_id='perform_docking',
+        image="alpine",
+        cmds=["sh", "-c", f'echo docking: barabra'],
+    )
 
-        prepare_ligands = KubernetesPodOperator(
-            namespace=namespace,
-            task_id='prepare_ligands',
-            image="alpine",
-            cmds=["sh", "-c", f'echo preparing: barabra'],
-        )
+    prepare_ligands = KubernetesPodOperator(
+        namespace=namespace,
+        task_id='prepare_ligands',
+        image="alpine",
+        cmds=["sh", "-c", f'echo preparing: barabra'],
+    )
 
-        prepare_ligands >> perform_docking
+    prepare_ligands >> perform_docking
         
     @task
     def get_batch_labels(db_label:str, n:int):
@@ -62,7 +60,7 @@ def test_dag():
 
     batch_labels = get_batch_labels(db_label='barabra', n=split_sdf.output)
 
-    docked = docking.expand(batch_label=batch_labels)
+    docked = prepare_ligands.expand(batch_label=batch_labels)
 
     postprocessing = BashOperator(
         task_id='postprocessing',
