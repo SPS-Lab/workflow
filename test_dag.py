fe0f2508feafa67a32b56f8a137fa8f528c6c480
Gabin Schieffer <gabin.schieffer@gmail.com>
2023-04-28 00:10:10 +0200

    test

--- test_dag.py
+++ test_dag.py
@@ -39,21 +39,23 @@ def test_dag():
     )
 
 
-    prepare_ligands = KubernetesPodOperator(
-        namespace=namespace,
-        task_id='prepare_ligands',
-        image="alpine",
-        cmds=["sh", "-c", f'echo preparing: barabra'],
-    )
-    perform_docking = KubernetesPodOperator(
-        namespace=namespace,
-        task_id='perform_docking',
-        image="alpine",
-        cmds=["sh", "-c", f'echo docking: barabra'],
-    )
+    
 
     @task_group
     def docking(batch_label: str):
+        prepare_ligands = KubernetesPodOperator(
+            namespace=namespace,
+            task_id='prepare_ligands',
+            image="alpine",
+            cmds=["sh", "-c", f'echo preparing: barabra'],
+        )
+        perform_docking = KubernetesPodOperator(
+            namespace=namespace,
+            task_id='perform_docking',
+            image="alpine",
+            cmds=["sh", "-c", f'echo docking: barabra'],
+        )
+
         prepare_ligands >> perform_docking
         
     @task
