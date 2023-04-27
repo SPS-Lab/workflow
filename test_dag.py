1e058b5b8b1f195f90d34352c346d85edaad9f3a
Gabin Schieffer <gabin.schieffer@gmail.com>
2023-04-27 23:41:28 +0200

    test

--- test_dag.py
+++ test_dag.py
@@ -68,7 +68,7 @@ def test_dag():
     def get_batch_labels(db_label, n):
         return [f'{db_label}_batch{i}.sdf' for i in range(n)]
 
-    batch_labels = get_batch_labels('db_label', split_sdf)
+    split_sdf >> get_batch_labels.partial(db_label='db_label')
 
     docked = docking.expand(batch_label=batch_labels)
 
