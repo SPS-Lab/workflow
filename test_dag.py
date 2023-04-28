b0cebf388ee8c8d7cdb268bd7788ee647d5aa99a
Gabin Schieffer <gabin.schieffer@gmail.com>
2023-04-28 10:16:59 +0200

    test

--- test_dag.py
+++ test_dag.py
@@ -57,8 +57,7 @@ def test_dag():
         def perform_docking(batch_label: str):
             print('perform_docking')
 
-        prepare_receptor             >> perform_docking(batch_label)
-        prepare_ligands(batch_label) >> perform_docking(batch_label)
+        prepare_receptor >> perform_docking(prepare_ligands(batch_label))
             
     """prepare_ligands = KubernetesPodOperator.partial(
         namespace=namespace,
