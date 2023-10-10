d4765d88cebe21f151e1d4c8830474a1b9e4a988
gabin-s <gabins@kth.se>
2023-10-10 14:13:29 +0200

    improved launch script for Airflow

--- misc/launch_airflow.sh
+++ misc/launch_airflow.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 helm repo add apache-airflow https://airflow.apache.org
+helm repo update apache-airflow
+
 helm upgrade --install airflow apache-airflow/airflow \
-	--namespace airflowgabin --create-namespace \
-	--set dags.persistence.enabled=false \
-	--set dags.gitSync.enabled=true \
- 	--set dags.gitSync.branch=main \
-	--set dags.gitSync.repo=https://github.com/SPS-Lab/workflow \
-	--set dags.gitSync.subPath=.
+	-f airflow-override-values.yaml \
+	--namespace airflow --create-namespace 
+
